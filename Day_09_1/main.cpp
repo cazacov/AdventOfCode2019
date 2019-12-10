084b3a6544304af2f2010b417e44e04a458bfbe7
Victor Cazacov <cazacov.git@gmail.com>
2019-12-10 23:52:51 +0100

    Day 9, problem 1

---
+++ Day_09_1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include "IntcodeComputer.h"
+
+int main() {
+
+    IntcodeComputer computer;
+    computer.load_program("program.txt");
+    computer.add_to_input(1);
+
+    do  {
+        bool has_out = computer.step(false);
+        if (has_out) {
+            std::cout << computer.get_last_output() << std::endl;
+        }
+    } while (!computer.is_halted());
+
+    return 0;
+}
