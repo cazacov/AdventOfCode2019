5a27ecfd7a42bfbf3447ec54eee232f781b715d6
Victor Cazacov <cazacov.git@gmail.com>
2019-12-18 00:44:02 +0100

    Explore the map

--- Day_15_1/Screen.h
+++ Day_15_1/Screen.h
@@ -8,6 +8,7 @@
 #include <string>
 
 enum Tile {
+    Unknown,
     Blank,
     Wall,
     Oxygen,
--- Day_15_1/main.cpp
+++ Day_15_1/main.cpp
@@ -8,21 +8,75 @@
 struct Pos {
     int x;
     int y;
+    Pos move(int direction) {
+        Pos result {x, y};
+        switch (direction) {
+            case 0:
+                result.y -=1;
+                break;
+            case 1:
+                result.y +=1;
+                break;
+            case 2:
+                result.x -=1;
+                break;
+            case 3:
+                result.x +=1;
+                break;
+        }
+        return result;
+    }
+
+    bool operator==(const Pos &other) const{
+        return x == other.x && y == other.y;
+    }
 };
+
+// Required by unordered_map
+template<> struct std::hash<Pos> {
+    std::size_t operator() (const Pos &node) const {
+        return (hash<int>()(node.x) << 4) ^ hash<int>()(node.y);
+    }
+};
+
 
 Screen screen;
 IntcodeComputer computer;
-
-Pos robot;
 
 int next_direction = 0;
 long on_input() {
     return next_direction;
 }
 
+Tile try_move(int direction);
+
+
+void explore(std::unordered_map<Pos, Tile> map, Pos robot) {
+
+    static int moves[4][2] { {0,1}, {0,-1}, {-1,0}, {1,0}};
+    for (int dir = 0; dir < 4; dir++) {
+
+
+
+
+        Tile result = try_move(di)
+
+    }
+}
+
+
+
 
 int main() {
     computer.load_program("program.txt");
+
+
+    std::unordered_map<Pos, Tile> map;
+
+    std::vector<int> path;
+
+
+
     std::vector<long> s {21201,1,1519,-2, 109,-3,2106,0, 0};
     int pos = 500;
     while (pos < computer.ram.size()-10) {
@@ -34,20 +88,21 @@ int main() {
         }
         if (i == s.size()) {
             std::cout << (pos + s.size()) << std::endl;
+            pos+=i;
             break;
         }
         pos++;
     }
 
     int columns = 16;
-    int len = 880;
+    int len = 890;
     std::string str;
 
     while (columns < 80) {
         screen.cls();
         for (int i = 0; i < len; i++) {
-            int x = i % columns;
-            int y = i / columns;
+            int x = i % columns + 1;
+            int y = i / columns + 1;
             switch(computer.ram[pos + i]) {
                 case 0:
                     screen.draw(x, y, Tile::Blank);
@@ -58,9 +113,11 @@ int main() {
                 case 2:
                     screen.draw(x, y, Tile::Oxygen);
                     break;
+                default:
+                    screen.draw(x, y, Tile::Robot);
             }
         }
-        std::cin >> str;
+        getchar();
         columns++;
     }
 
@@ -68,21 +125,26 @@ int main() {
     return 0;
 }
 
-/*
-Tile try_move(int direction) {
 
-static int moves[4][2] { {0,1}, {0,-1}, {-1,0}, {1,0}};
+Tile try_move(int direction) {
 
     Tile result;
     next_direction = direction;
-    if (computer.step(false, on_input)) {
-        switch(computer.get_last_output()) {
-            case 0:
-                result = Tile::Wall;
-                break;
-            case 1:
-        }
 
+
+    while (!computer.step(false, on_input)) {
+        ;
     }
+    switch(computer.get_last_output()) {
+        case 0:
+            result = Tile::Wall;
+            break;
+        case 1:
+            result = Tile::Blank;
+            break;
+        case 2:
+            result = Tile::Oxygen;
+            break;
+    }
+    return result;
 }
-*/
