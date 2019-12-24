6d88774218c957f68c165d472a1a9c2ef03727b8
Victor Cazacov <cazacov.git@gmail.com>
2019-12-24 20:22:36 +0100

    Day 19, problem 2 solved

--- Day_19_1/main.cpp
+++ Day_19_1/main.cpp
@@ -9,43 +9,11 @@
 
 using namespace std;
 
-struct Pos {
-    int x;
-    int y;
-    Pos move(int direction) const {
-        Pos result {x, y};
-        switch (direction) {
-            case 0:
-                result.y -=1;
-                break;
-            case 1:
-                result.y +=1;
-                break;
-            case 2:
-                result.x -=1;
-                break;
-            case 3:
-                result.x +=1;
-                break;
-        }
-        return result;
-    }
-
-    bool operator==(const Pos &other) const{
-        return x == other.x && y == other.y;
-    }
-
-    bool operator!=(const Pos &other) const{
-        return x != other.x || y != other.y;
-    }
+struct BeamWidth {
+    int from;
+    int to;
 };
 
-// Required by unordered_map
-template<> struct std::hash<Pos> {
-    std::size_t operator() (const Pos &node) const {
-        return (hash<int>()(node.x) << 4) ^ hash<int>()(node.y);
-    }
-};
 
 long get_status(IntcodeComputer &computer, int x, int y);
 
@@ -60,19 +28,27 @@ IntcodeComputer computer;
 int main() {
     Screen screen;
     computer.load_program("program.txt");
-    std::unordered_map<Pos, bool> map;
+    vector<BeamWidth> beam;
     screen.cls();
 
     int affected_points = 0;
 
     int top = 0;
-    for (int x = 0; x < 50; x++) {
+    for (int x = 0; x < 500000; x++) {
+        BeamWidth bw;
         bool beam_found = false;
-        for (int y = 0; y < 50; y++) {
+        int limit_from = 0;
+        int limit_to = x;
+        if (x > 10) {
+            limit_from = top;
+            limit_to = INT32_MAX;
+        }
+        for (int y = limit_from; y < limit_to; y++) {
             long result = get_status(computer, x, y);
             if (result == 0) {
-                screen.text(x+1,y+1, ".");
+//                screen.text(x+1,y+1, ".");
                 if (beam_found) {
+                    bw.to = y - 1;
                     break;
                 }
                 else if (top < y) {
@@ -80,11 +56,27 @@ int main() {
                 }
             }
             else {
-                beam_found = true;
+                if (!beam_found) {
+                    beam_found = true;
+                    bw.from = y;
+                }
                 affected_points++;
-                screen.text(x+1,y+1, "#");
+//                screen.text(x+1,y+1, "#");
             }
         }
+        beam.push_back(bw);
+        cout << x << "\t" << bw.from << "\t" << bw.to << std::endl;
+        // check square
+        if (bw.to - bw.from > 100 && beam.size() > 100) {
+            int candidate_to = bw.from + 99;
+            if (beam[x-99].to >= candidate_to) {
+                // Square found!
+                cout << (x - 99)*10000 + bw.from;
+                return 0;
+            }
+        }
+
+
     }
     cout << endl << affected_points;
 }
